c095f138cc58aa8e012554acbe632a53e87a031b
Niroshan <niroshan05062001@gmail.com>
2022-07-13 22:04:49 +0530

    Add project files.

---
+++ Team2_LMS/Models/EmployeeDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team2_LMS.Models
+{
+    public class EmployeeDB
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string E_Mail { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public long ContactNumber { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateJoined { get; set; }
+        [Required]
+        public int LeaveBalance { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Possword { get; set; }
+        
+    }
+}
---
+++ Team2_LMS/Models/EmployeeModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Team2_LMS.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string E_Mail { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public long ContactNumber { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateJoined { get; set; }
+        [Required]
+        public int LeaveBalance { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Possword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string CheckPassword { get; set; }
+    }
+}
